3d3abc97faabcfb86cb02318019804694eb8aeb4
avsjanka <aacash.1917@gmail.com>
2022-04-02 21:51:30 +0700

    Добавлены case - шифров и  enum

--- РГР_Кашевская/РГР_Кашевская.cpp
+++ РГР_Кашевская/РГР_Кашевская.cpp
@@ -1,11 +1,45 @@
-﻿// РГР_Кашевская.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
 using namespace std;
+enum CIPHER { NOTEBOOK = 0, VERNAM = 1, CAESAR = 2, RSA = 3, VIGENERE = 4, LAST = 5 };
 
 int main()
 {
-    cout << "Hello World!\n";
+    int number = 0;
+    cout << "Write number of cipher";
+    cin >> number;
+    switch (number)  // switch/case дяя номеров шифров
+    {
+        case NOTEBOOK :
+        {
+
+            break;
+        }
+        case VERNAM :  
+        {
+
+            break;
+        }
+        case CAESAR :
+        {
+            break;
+        }
+        case RSA :
+        {
+            break;
+        }
+        case VIGENERE :
+        {
+            break;
+        }
+        case LAST :
+        {
+            break;
+        }
+        default :
+            break;
+
+    }
+
 }
 

afa1291c09707b6163bb81ad455e52aa04134c32
Avjanka <Avjanka@LAPTOP-V8J2M6HA>
2022-04-02 19:13:41 +0700

    Добавьте файлы проекта.

---
+++ РГР_Кашевская/РГР_Кашевская.cpp
@@ -0,0 +1,11 @@
+﻿// РГР_Кашевская.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+

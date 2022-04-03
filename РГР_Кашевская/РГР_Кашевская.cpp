d4878da9f1b63282938824e4e200e95fb0bee860
avsjanka <aacash.1917@gmail.com>
2022-04-04 01:34:05 +0700

    Добавлено заполнение алфавита в подстановочном шифре NOTEBOOK и перемешивание позиций в стоках методом rand_shuffle, а также перемешивание самих строк.

--- РГР_Кашевская/РГР_Кашевская.cpp
+++ РГР_Кашевская/РГР_Кашевская.cpp
@@ -1,18 +1,42 @@
 ﻿
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-enum CIPHER { NOTEBOOK = 0, VERNAM = 1, CAESAR = 2, RSA = 3, VIGENERE = 4, LAST = 5 };
+enum CIPHER { NOTEBOOK = 0, VERNAM = 1, CAESAR = 2, RSA = 3, VIGENERE = 4, HILL = 5 };
 
 int main()
 {
     int number = 0;
-    cout << "Write number of cipher";
+    cout << "Write number of cipher ";
     cin >> number;
     switch (number)  // switch/case дяя номеров шифров
     {
         case NOTEBOOK :
         {
-
+            char alphabet[] = "abcdefgijhklmnopqrstuvwxyz .ABCDEFGIHJKLMNOPQRTSUVWXYZ0123456789,!?;:()-";
+            vector < vector <char>> vec_alp(10, vector<char>(10));
+            for (int i = 0, k = 0; i<10 ; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (alphabet[k] != '\0')
+                        vec_alp[i][j] = alphabet[k];
+                    else
+                        break;
+                    k++;
+                    if (k == 72)
+                        k = 0;
+                }
+            }
+            random_shuffle(vec_alp.begin(), vec_alp.end());
+            for (vector <char> element : vec_alp)
+            {
+                random_shuffle(element.begin(), element.end());
+                for (char symbol : element)
+                    cout << symbol;
+                cout << endl;
+            }
             break;
         }
         case VERNAM :  
@@ -32,7 +56,7 @@ int main()
         {
             break;
         }
-        case LAST :
+        case HILL :
         {
             break;
         }

26ff1b392b7dfcb072a38fcfb940f1d57edea081
avsjanka <aacash.1917@gmail.com>
2022-04-04 22:29:34 +0700

    Добавление потока ввода сообщения в файл РГР.txt

--- РГР_Кашевская/РГР_Кашевская.cpp
+++ РГР_Кашевская/РГР_Кашевская.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
-#define FILE_NAME ""
+#include <fstream>
+#define FILE_NAME "C:\\Users\\пользователь\\Desktop\\РГР.txt"
 using namespace std;
 enum CIPHER { NOTEBOOK = 0, VERNAM = 1, CAESAR = 2, RSA = 3, VIGENERE = 4, HILL = 5 };
 
@@ -26,9 +26,23 @@ vector<int> find_symbol(vector<vector <char>> key, char symbol)
 
 int main()
 {
-    int number = 0;
+    int number = 0; 
+    int ind = 0;
+    char in_char, out_char;
+    vector<char> letter;
     cout << "Write number of cipher ";
     cin >> number;
+    cout << "Write your letter: " << endl;
+    ofstream input;
+    input.open(FILE_NAME, ios::out);
+    while (cin >> in_char)        
+        input << in_char;
+    input.close();
+    ifstream output;
+    output.open(FILE_NAME, ios::in);
+    while( output >> out_char)
+        letter.push_back(out_char);
+    output.close();
     switch (number)  // switch/case дяя номеров шифров
     {
         case NOTEBOOK :
@@ -50,11 +64,7 @@ int main()
             }
             random_shuffle(vec_alp.begin(), vec_alp.end());
             for (vector <char> element : vec_alp)
-            {
                 random_shuffle(element.begin(), element.end());
-            }
-            for (int element: find_symbol(vec_alp, 'a'))
-                cout << element;
             break;
         }
         case VERNAM :  
@@ -82,6 +92,5 @@ int main()
             break;
 
     }
-
 }
 

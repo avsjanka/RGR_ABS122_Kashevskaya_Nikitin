434ac742938ff5d627202f05284d14ea11c130ee
Саша Никитин <future200310@gmail.com>
2022-04-13 23:40:19 +0700

    Начата работа над шифром Вижинера, реализованы скелеты функций

--- РГР_Кашевская/РГР_Кашевская.cpp
+++ РГР_Кашевская/РГР_Кашевская.cpp
@@ -1,7 +1,48 @@
 ﻿
 #include <iostream>
+#include <string>
+#include <locale>
+#include <vector>
 using namespace std;
 enum CIPHER { NOTEBOOK = 0, VERNAM = 1, CAESAR = 2, RSA = 3, VIGENERE = 4, HILL = 5 };
+
+char rus_encrypt(char letter)
+{
+    char encrypted_letter = 'M';
+    return encrypted_letter;
+}
+
+char eng_encrypt(char letter)
+{
+    char encrypted_letter = 'И';
+    return encrypted_letter;
+}
+
+void vigenere(const string message, const string key)
+{
+    setlocale(LC_ALL, "Ru");//32 - 126 Eng  -64 - -1 Рус
+    vector<char> encrypt_mes, full_key(message.size());
+    if (message.size() <= key.size())
+    {
+        for(int i = 0, k = 0; i <)
+    }
+    for (char letter : message)
+    {
+        if (int(letter) > 31 && int(letter) < 127)
+        {
+            encrypt_mes.push_back(eng_encrypt(letter));
+        }
+        else if (int(letter) > -63 && int(letter) < 0)
+        {
+            encrypt_mes.push_back(rus_encrypt(letter));
+        }
+    }
+    for (char letter : encrypt_mes)
+    {
+        cout << letter;
+    }
+}
+
 
 int main()
 {
@@ -37,6 +78,7 @@ int main()
         }
         case VIGENERE :
         {
+            vigenere("stringытппыктп", "key");
             break;
         }
         case HILL :

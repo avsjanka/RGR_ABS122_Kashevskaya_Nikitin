802fdabd7c502e5aea45c3b2f405c30f09020d84
avsjanka <aacash.1917@gmail.com>
2022-05-04 22:33:44 +0700

    Добавлена шифр цезаря по криптопротоколу Дитффи-Хелмана. РАботает для латиницы с учётом регистров. Для кирилицы алгоритм неверен из-за различий кодировок.

--- РГР_Кашевская/РГР_Кашевская.cpp
+++ РГР_Кашевская/РГР_Кашевская.cpp
@@ -1,5 +1,6 @@
 ﻿
 #include <iostream>
+#include <random>
 #include <algorithm>
 #include <vector>
 #include <fstream>
@@ -24,8 +25,92 @@ vector<int> find_symbol(vector<vector <char>> key, char symbol)
         return value;
 }
 
+int vernam_key(int length)
+{
+    int key = 0;
+    for (int i = 0; i <= length; i++)
+        key = key + rand() % 2;
+    return key;
+}
+
+int remaind_of_div(int a, int x, int p)
+{
+    int remaind = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        remaind *= a;
+        remaind %= p;
+    }
+    return remaind;
+}
+
+int chip(int p, int g)
+{
+    int xa = 7, xb = 13;
+    int Ya = 0;
+    Ya = remaind_of_div(g, xa, p);
+    return  remaind_of_div(Ya, xb, p);
+}
+
+int dechip(int p, int g)
+{
+    int xa = 7, xb = 13;
+    int Yb = 0;
+    Yb = remaind_of_div(g, xb, p);
+    return remaind_of_div(Yb, xa, p);
+}
+
+vector<char> caesar_chip(vector<char> letter)
+{
+    int shift = chip(821, 97);
+    vector<char> chip_letter;
+    for (char element : letter)
+    {
+        if (element >= 'A' && element <= 'Z' || element >= 'a' && element <= 'z')
+        {
+            int chr_int = (int)element + (shift % 26);
+            if (chr_int > (int)'Z' && element >= 'A' && element <= 'Z')
+            {
+                chr_int -= 25;
+                element = (char)chr_int;
+            }
+            else if (chr_int <= (int)'Z' && element >= 'A' && element <= 'Z')
+                element = (char)chr_int;
+            if( chr_int > (int)'z' && element >= 'a' && element <= 'z')
+            {
+                chr_int -= 25;
+                element = (char)chr_int;
+            }
+            else if (chr_int <= (int)'z' && element >= 'a' && element <= 'z')
+                element = (char)chr_int;
+        }
+        if (element >= 'А' && element <= 'Я' || element >= 'а' && element <= 'я') 
+        {
+            int chr_int = (int)element - (shift % 33);
+            if (chr_int > (int)'А' && element >= 'А' && element <= 'Я')
+            {
+                chr_int += 32;
+                element = (char)chr_int;
+            }
+            else if (chr_int <= (int)'Я' && element >= 'А' && element <= 'Я')
+                element = (char)chr_int;
+            if (chr_int > (int)'я' && element >= 'а' && element <= 'я')
+            {
+                chr_int += 32;
+                element = (char)chr_int;
+            }
+            else if (chr_int <= (int)'я' && element >= 'а' && element <= 'я')
+                element = (char)chr_int;
+        }
+        chip_letter.push_back(element);
+    }
+    return chip_letter;
+}
+   
+
 int main()
 {
+    setlocale(LC_ALL, "ru");
     int number = 0; 
     int ind = 0;
     char in_char, out_char;
@@ -43,11 +128,11 @@ int main()
     while( output >> out_char)
         letter.push_back(out_char);
     output.close();
-    switch (number)  // switch/case дяя номеров шифров
+    switch (number) 
     {
         case NOTEBOOK :
         {
-            char alphabet[] = "abcdefgijhklmnopqrstuvwxyz .ABCDEFGIHJKLMNOPQRTSUVWXYZ0123456789,!?;:()-";
+            char alphabet[] = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ .ABCDEFGIHJKLMNOPQRTSUVWXYZ0123456789,!?;:()-";
             vector < vector <char>> vec_alp(10, vector<char>(10));
             for (int i = 0, k = 0; i<10 ; i++)
             {
@@ -58,7 +143,7 @@ int main()
                     else
                         break;
                     k++;
-                    if (k == 72)
+                    if (k == 80)
                         k = 0;
                 }
             }
@@ -69,11 +154,13 @@ int main()
         }
         case VERNAM :  
         {
+            int key = vernam_key(((int) letter.size()) * 8);
 
             break;
         }
         case CAESAR :
         {
+            vector<char> result = caesar_chip(letter);
             break;
         }
         case RSA :

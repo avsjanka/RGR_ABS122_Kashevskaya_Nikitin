3b2a31c32237b57d041dd8e356b2b6380f88e7e8
avsjanka <aacash.1917@gmail.com>
2022-06-02 23:05:54 +0700

    02.06.2020 fix input_vector.cpp

--- РГР_Кашевская/input_vector.cpp
+++ РГР_Кашевская/input_vector.cpp
@@ -8,34 +8,75 @@ vector<char> input_vector(int number)
     vector<char> letter;
     if (number == 1)//Notebook
     {
+        cin.clear();
+        cin.ignore(std::cin.rdbuf()->in_avail());
         string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz .0123456789,!?;:()-'\n";
         char symbol;
         cout << "Please, write your letter from this alphabet: " << alphabet;
         while (cin.get(symbol))
         {
-            letter.push_back(symbol);
+            if (alphabet.find_first_of(symbol) == string::npos)
+            {
+                if (symbol == '\x1a')
+                {
+                    continue;
+                }
+                else
+                {
+                    cout << "This letter is incorrect!" << endl;
+                    cin.clear();
+                    cin.ignore(std::cin.rdbuf()->in_avail());
+                    return letter;
+                }
+            }
+            else
+            {
+                letter.push_back(symbol);
+            }
         }
         cin.clear();
     }
     else if (number == 2)//Vernam
     {
         char symbol;
+        cout << "Please, write your letter: ";
         while (cin.get(symbol))
         {
             letter.push_back(symbol);
         }
         cin.clear();
+        cin.ignore(std::cin.rdbuf()->in_avail());
     }
     else//Caesar
     {
+        cin.clear();
+        cin.ignore(std::cin.rdbuf()->in_avail());
         string alphabet = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏĞÑÒÓÔÕÖ×ØÙÚÛÜİŞßàáâãäåæçèéêëìíîïğñòóôõö÷øùúûüışÿ ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         char symbol;
         cout << "Please, write your letter from this alphabet: " << alphabet << endl;
         while (cin.get(symbol))
         {
-            letter.push_back(symbol);
+            if (alphabet.find_first_of(symbol) == string::npos)
+            {
+                if (symbol == '\x1a')
+                {
+                    continue;
+                }
+                else
+                {
+                    cout << "This letter is incorrect!" << endl;
+                    cin.clear();
+                    cin.ignore(std::cin.rdbuf()->in_avail());
+                    return letter;
+                }
+            }
+            else
+            {
+                letter.push_back(symbol);
+            }
         }
-        cin.clear();
     }
+    cin.clear();
+    cin.ignore(std::cin.rdbuf()->in_avail());
     return letter;
 }
--- РГР_Кашевская/input_vector.h
+++ РГР_Кашевская/input_vector.h
@@ -1,8 +1,11 @@
 #pragma once
+#define NOMINMAX
 #include <iostream>
 #include <string>
 #include <locale>
 #include <vector>
+#include <numeric>
+#include <limits>
 #include <Windows.h>
 using namespace std;
-vector<char> input_vector(int number);
+vector<char> input_vector(int num);
